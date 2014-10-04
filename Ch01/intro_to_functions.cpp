fb884a65116d98d0de60113a7beaad0500bb0a88
Nissim Nabar <nissimnabar@gmail.com>
2014-10-04 18:04:14 +0530

    Added a General Info text file that gives perspective on how to use functions in C++

--- Ch01/intro_to_functions.cpp
+++ Ch01/intro_to_functions.cpp
@@ -12,6 +12,13 @@ int DoPrint() {
 // Defining the main function as always :)
 // Functions can be invoked multiple times as well
 
+// The function add takes two integers as parameters 
+
+
+int add(int x, int y){
+	return x + y ;
+}
+
 int main() {
 	cout <<"Currently in main() " << endl ;
 	// Using a for loop
@@ -20,9 +27,12 @@ int main() {
 		cout << i + 1 << endl ;
 		DoPrint() ;
 	}
+
 //	DoPrint() ;
 //	DoPrint() ;
 	cout << "Back in main() " << endl ;
-
+	// Initializing and defining an integer y to hold the output of add()
+	int y = add(4,5) ;
+	cout << y << endl ;
 	return 0 ;
 }
