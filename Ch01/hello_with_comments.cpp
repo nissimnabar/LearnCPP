1328c0a3e5c3a8fed25658f584f26396f85fc31d
Nissim Nabar <nissimnabar@gmail.com>
2014-10-04 16:58:04 +0530

    Using cin to get an input of a number

--- Ch01/hello_with_comments.cpp
+++ Ch01/hello_with_comments.cpp
@@ -8,5 +8,12 @@ int main()
 {
 	cout << "Hello World! " << endl ; // Now we need not put std:: 
 	cout << 2 + 3 << endl; // Calculating the sum of 2 and 3
+
+	int x ;
+	cout << "Enter an integer " << endl ;
+	cin >> x ;
+	cout << "You entered " << x <<endl ;
+
+
 	return 0 ; // Return value of 0 means everything went OK
 }
