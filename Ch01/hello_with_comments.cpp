7a96bc9143a9fd27de2982e30912ce6e976e6f08
Nissim Nabar <nissimnabar@gmail.com>
2014-10-04 16:52:38 +0530

    Initial Commit

---
+++ Ch01/hello_with_comments.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Preprocessor directive
+
+using namespace std ; //Defining the namespace
+
+// Every C++ program must have a main() function 
+
+int main()
+{
+	cout << "Hello World! " << endl ; // Now we need not put std:: 
+	return 0 ; // Return value of 0 means everything went OK
+}
