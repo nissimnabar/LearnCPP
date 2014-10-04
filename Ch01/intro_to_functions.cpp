909c94872d26152b7ec0f34ef9f562f252bdea5c
Nissim Nabar <nissimnabar@gmail.com>
2014-10-04 17:11:19 +0530

    Added comments to intro_to_functions.cpp

--- Ch01/intro_to_functions.cpp
+++ Ch01/intro_to_functions.cpp
@@ -2,10 +2,14 @@
 
 using namespace std ;
 
+// Defining a function to print its own name :P
+
 int DoPrint() {
 	cout << "Running DoPrint" << endl ;
 	return 0 ;
 }
+
+// Defining the main function as always :)
 
 int main() {
 	cout <<"Currently in main() " << endl ;
