a8dbe538332409a3e37ffce6cc76002bb21b5965
Nissim Nabar <nissimnabar@gmail.com>
2014-10-04 18:19:31 +0530

    Created two files add.cpp and main.cpp to hold the add function. Testing forward definition with multiple files

--- Ch01/intro_to_functions.cpp
+++ Ch01/intro_to_functions.cpp
@@ -19,6 +19,10 @@ int add(int x, int y){
 	return x + y ;
 }
 
+// Forward definition of the multiply function 
+
+int multiply(int a, int b) ;
+
 int main() {
 	cout <<"Currently in main() " << endl ;
 	// Using a for loop
@@ -34,5 +38,12 @@ int main() {
 	// Initializing and defining an integer y to hold the output of add()
 	int y = add(4,5) ;
 	cout << y << endl ;
+
+	int z = multiply(add(2,3), add(4,5)) ;
+	cout << add(2,3) << " x "<< add(4,5) << " = "<< z << endl ;
 	return 0 ;
+}
+
+int multiply(int a, int b){
+	return a*b ;
 }
---
+++ Ch01/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std ;
+int add (int x, int y) ;
+
+int main(){
+	cout << "The sum of 3 and 5 is: " << add(3,5) << endl ;
+	return 0;
+}
