344732bdeaf5b249cdae6ec069708a84e0aece72
Nissim Nabar <nissimnabar@gmail.com>
2014-10-04 18:31:01 +0530

    Added a comment about the header guard

---
+++ Ch01/add.h
@@ -0,0 +1,10 @@
+// This is a header file for the add function
+
+#ifndef ADD_H
+#define ADD_H 
+// This is known as a header guard so that 
+// same files do not get added multiple times
+
+int add(int x, int y) ; //function prototype for the add function
+
+#endif
